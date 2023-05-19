e483972bb126c2f13f34bb0281c639a7b05eca77
dsilvas2001 <dsilvas@uteq.edu.ec>
2023-05-19 14:24:15 -0500

    Componente

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ module.exports = {
   e2e: {
     setupNodeEvents() {
       // implementa aquí tus eventos de Node.js si es necesario
-    }
-  }
-}
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+};
